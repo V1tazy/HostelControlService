4c4ddc8ae864e646fc724d053673129b5033ab65
V1tazy <slavasenotrusov2005@gmail.com>
2024-05-15 02:53:53 +0900

    Created new Class VIewModel

---
+++ ViewModel/AdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HostelControlService.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelControlService.ViewModel
+{
+    internal class AdminViewModel: ViewModelBase
+    {
+    }
+}
